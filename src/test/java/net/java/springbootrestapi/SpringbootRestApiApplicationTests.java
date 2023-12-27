92c5e59130846baf42f003f64d2c4276bb1f6366
johnrodonnell <matadorcrab@gmail.com>
2023-12-26 22:40:21 -0600

    init

---
+++ src/test/java/net/java/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.java.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
